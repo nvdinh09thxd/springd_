81f80d6d76ee088be3161b1e9ff92b7f70984f2f
nvdinh09thxd <nvdinh09thxd@gmail.com>
2021-11-06 16:34:35 +0700

    template project

---
+++ src/main/java/edu/vinaenter/configs/ApplicationContextConfig.java
@@ -0,0 +1,19 @@
+package edu.vinaenter.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScans;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScans({@ComponentScan(value = "edu.vinaenter.*")})
+public class ApplicationContextConfig {
+	@Bean("viewResolver")
+    public InternalResourceViewResolver getViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/pages/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
